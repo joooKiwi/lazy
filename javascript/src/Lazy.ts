4a0623c2f73921fdb94f97e801309fb63af629e8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-06-27 22:02:47 -0400

    Addition of the usable files to implement a lazy object holder

---
+++ javascript/src/Lazy.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023. Jonathan Bédard ~ JóôòKiwi
+ *
+ * This project is free to use.
+ * All the right is reserved to the author of this project.
+ */
+
+/**
+ * Represent a value with a possibly lazy value
+ *
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-lazy/ Kotlin lazy
+ */
+export interface Lazy<out T, > {
+
+    /** Tell if the {@link value} has been initialized */
+    get isInitialized(): boolean
+
+    /** The value held by the {@link Lazy} instance */
+    get value(): T
+
+}
