ad91e65bc5ab5ad8b8dc90f3c8fea31c253beba0
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-06-27 22:05:25 -0400

    Addition of tests associated to the source file

---
+++ javascript/test/InitializedLazy.test.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023. Jonathan Bédard ~ JóôòKiwi
+ *
+ * This project is free to use.
+ * All the right is reserved to the author of this project.
+ */
+
+import {InitializedLazy} from "../src/InitializedLazy"
+import {everyConstant}   from "./constants"
+
+describe("InitializedLazyTest", () => {
+    describe.each(everyConstant,)("%s", ({value: it,},) => {
+        test("value", () => expect(new InitializedLazy(it,).value,).toBe(it,),)
+        describe("is initialized ", () => {
+            test("before get", () => expect(new InitializedLazy(() => it,).isInitialized,).toBeTrue(),)
+            test("after get", () => {
+                const lazy = new InitializedLazy(() => it,)
+                lazy.value
+                expect(lazy.isInitialized,).toBeTrue()
+            },)
+        },)
+        describe("toString does not throw", () => {
+            test("before get", () => expect(() => new InitializedLazy(it,).toString(),).not.toThrow(),)
+            test("after get", () => {
+                const lazy = new InitializedLazy(it,)
+                lazy.value
+                expect(() => lazy.toString(),).not.toThrow()
+            },)
+        })
+    },)
+},)
---
+++ javascript/test/jestExtended.d.ts
@@ -0,0 +1,8 @@
+/******************************************************************************
+ * Copyright (c) 2023. Jonathan Bédard ~ JóôòKiwi                             *
+ *                                                                            *
+ * This project is free to use.                                               *
+ * All the right is reserved to the author of this project.                   *
+ ******************************************************************************/
+
+import 'jest-extended'
