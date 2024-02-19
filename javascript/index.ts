db8033017e8f68d2385954df98e5d1e2b1d3fb82
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-19 14:33:30 -0500

    Update to the copyright to 2024

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,1 +1,8 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
 export * from "./src/index"
--- javascript/jest.config.ts
+++ javascript/jest.config.ts
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
 import type {Config} from "jest"
 
 /*
