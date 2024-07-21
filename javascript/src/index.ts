519fe2e6aeadc2e62bc2dcaf57c9a8b9f4b25969
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-07-21 12:51:29 -0400

    Addition of the imports for the new specialized "lazyOf"

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -5,7 +5,10 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-export {CommonLazy} from "./CommonLazy"
-export type {Lazy}  from "./Lazy"
-export {lazy}       from "./method/lazy"
-export {lazyOf}     from "./method/lazyOf"
+export {CommonLazy}    from "./CommonLazy"
+export type {Lazy}     from "./Lazy"
+export {bigIntLazyOf}  from "./method/bigIntLazyOf"
+export {booleanLazyOf} from "./method/booleanLazyOf"
+export {lazy}          from "./method/lazy"
+export {lazyOf}        from "./method/lazyOf"
+export {numberLazyOf}  from "./method/numberLazyOf"
