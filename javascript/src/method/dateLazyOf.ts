7784c27599e0f36b4fdb0dccd8420f3f88c01cb0
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-07 18:16:14 -0500

    Addition of tests for the specialized "lazyOf" for the "Date"

---
+++ javascript/src/method/dateLazyOf.ts
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+import type {Lazy} from "../Lazy"
+
+import {CommonLazy}      from "../CommonLazy"
+import {InitializedLazy} from "../InitializedLazy"
+
+/**
+ * Retrieve a value that is a {@link Date} from the {@link CommonLazy} (if it exists)
+ * or create a new {@link Lazy} from an already initialized {@link value}
+ *
+ * @param value The {@link Date} value directly
+ * @see lazyOf
+ */
+export function dateLazyOf<const T extends Date, >(value: T,): Lazy<T>
+export function dateLazyOf(value: Date,): Lazy<Date> {
+    const time = value.getTime()
+    if (Number.isNaN(time,))
+        return CommonLazy.INVALID_DATE
+    if (time === 0)
+        return CommonLazy.EPOCH_DATE
+    return new InitializedLazy(value,)
+}
